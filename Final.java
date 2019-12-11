11142495cb0762bfaa255b0a843eb46cb27f4ba1
DaniSaurio95 <58769958+DaniSaurio95@users.noreply.github.com>
2019-12-11 11:17:34 +0100

    Update Final.java

--- Final.java
+++ Final.java
@@ -1,22 +1,56 @@
 package FInal;
 
-public class Final {
+import java.util.Scanner;
 
+public class final {
+	
+	final public static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		
-		
-		
+
+
+		int row,col,B1,B2;
+		row = askRow();
+		col = askCol();
+		B1 = askB1();
+		B2 = askB2();
+
 		int board1[][]= generateBoard(3,3);
 		int board2[][]= generateBoard(3,3);
 		boolean visual1[][]= generateBoolean(3,3);
 		boolean visual2[][]= generateBoolean(3,3);
-
 	}
 
-	
+
+	public static int askRow() {
+		int row;
+		System.out.println("Introduce the number of rows: ");
+		row = sc.nextInt();
+		return row;
+	}
+	public static int askCol() {
+		int col;
+		System.out.println("Introduce the number of columns: ");
+		col = sc.nextInt();
+		return col;
+	}
+	public static int askB1() {
+		int B1;
+		System.out.println("Introduce the number of boats of length 1: ");
+		B1 = sc.nextInt();
+		return B1;
+	}
+	public static int askB2() {
+		int B2;
+		System.out.println("Introduce the number of boats of length 2: ");
+		B2 = sc.nextInt();
+		return B2;
+	}
+
+
 	public static int[][]generateBoard(int a,int b){
 		int array1[][]=new int[a][b];
-				
+
 		return array1;
 	}
 	public static boolean[][]generateBoolean(int a, int b) {
@@ -29,10 +63,4 @@ public class Final {
 		
 		return array2;
 	}
-	
-	
-	
-	
-	
-	
 }
